cc98ca4e13a3468e0a4b26ae5078731982857856
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2024-08-17 15:09:59 -0400

    initial commit

---
+++ conda_bonjour/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for conda-bonjour."""
+
+__author__ = "Bolun Dai"
+__email__ = "daibolun19950216@gmail.com"
+__version__ = "0.0.1"
+
+from .common import hello_world, numpy_hello_world
